fa4200916fbb6030af5540dee00dbf42c2ac5d43
Moritz Lenz <moritz@cpan.org>
2010-08-19 12:29:45 +0000

    Import of MORITZ/File-CountLines-v0.0.3 from CPAN.
    gitpan-cpan-distribution: File-CountLines
    gitpan-cpan-version:      v0.0.3
    gitpan-cpan-path:         MORITZ/File-CountLines-v0.0.3.tar.gz
    gitpan-cpan-author:       MORITZ
    gitpan-cpan-maturity:     released

--- lib/File/CountLines.pm
+++ lib/File/CountLines.pm
@@ -2,7 +2,7 @@ package File::CountLines;
 use strict;
 use warnings;
 
-our $VERSION = '0.0.2';
+our $VERSION = '0.0.3';
 our @EXPORT_OK = qw(count_lines);
 
 use Exporter 5.057;
@@ -185,7 +185,7 @@ If there's demand for, I can implement that though.
 
 You can add your own EOL styles by adding them to the
 C<%File::CountLines::StyleMap> hash, with the name of the style as hash key
-and the seperator as the value.
+and the separator as the value.
 
 =head1 AUTHOR
 
@@ -201,9 +201,9 @@ Domain.
 
 =head1 DEVELOPMENT
 
-You can obtain the latest development version via subversion:
+You can obtain the latest development version from L<http://github.com/moritz/File-CountLines>:
 
-    svn co https://faui2k3.org/svn/moritz/cpan/File-CountLines/
+    git clone git://github.com/moritz/File-CountLines.git
 
 =cut
 
